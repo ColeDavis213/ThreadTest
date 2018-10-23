fdac60aea9e96740be59024610eb08c5bc5979e2
ColeDavis213 <coledavi@ut.utm.edu>
2018-10-23 14:17:42 -0500

    not wokring for some reason

---
+++ ThreadTest/FindPiThread.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+//For Test: Wizards, Orthagonality, Pride and Predijuce Chapter, Databases in WPF, software development life cycle(waterfall), structual/behavior patt
+//UMLS, Creational???
+
+namespace ThreadTest
+{
+    class FindPiThread
+    {
+        public FindPiThread(int darts)
+        {
+            dartsToThrow = darts;
+            dartsLanded = 0;
+        
+            throws = new Random();
+        }
+
+        public int getLanded()
+        {
+            return dartsLanded;
+        }
+
+        public void throwDart()
+        {
+            double x = throws.NextDouble();
+            double y = throws.NextDouble();
+
+            for (int i = 0; i < dartsToThrow; i++)
+            {
+                if (1 <= ((x * x) + (y * y)))
+                {
+                    dartsLanded += 1;
+                }
+            }
+            
+        }
+
+        private int dartsToThrow;
+        private int dartsLanded;
+        private Random throws;
+    }
+}
--- ThreadTest/Program.cs
+++ ThreadTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThreadTest
 {
@@ -10,6 +11,53 @@ namespace ThreadTest
     {
         static void Main(string[] args)
         {
+            int darts, threads;
+            int dartsInside = 0;
+
+            Console.WriteLine("How many darts to throw: ");
+
+            darts = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many threads to run: ");
+
+            threads = Convert.ToInt32(Console.ReadLine());
+
+            List<Thread> threadList = new List<Thread>(threads);
+            List<FindPiThread> findPiList = new List<FindPiThread>(threads);
+
+            for(int i = 0; i < threads; i++)
+            {
+                FindPiThread findPiTest = new FindPiThread(darts);
+
+                findPiList.Add(findPiTest);
+
+                Thread threadTest = new Thread(new ThreadStart(findPiTest.throwDart));
+
+                threadList.Add(threadTest);
+
+                threadTest.Start();
+
+                Thread.Sleep(16);
+            }
+
+            for(int i = 0; i < threadList.Count; i++)
+            {
+                threadList[i].Join();
+            }
+
+            for(int i = 0; i < findPiList.Count; i++)
+            {
+                Console.WriteLine("get landed: {0}", findPiList[i].getLanded());
+                dartsInside += findPiList[i].getLanded();
+            }
+
+            double pi = (4 * ((dartsInside) / (darts)));
+
+            Console.WriteLine("Darts Inside: {0} Darts: {1} Pi: {2}", dartsInside, darts, pi);
+
+            Console.ReadKey();
+
+
         }
     }
 }
